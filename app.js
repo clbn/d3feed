0f023df920b2c14f6ac3376806fa246418f980a1
Alex Olshansky <i@creagenics.com>
2012-08-11 17:43:00 +0400

    Add simple proxy to Dirty.ru feed

--- app.js
+++ app.js
@@ -1,12 +1,32 @@
 var express = require('express');
 var app = express();
 
+var http = require('http');
+
 app.get('/', function(req, res) {
   res.send('d3feed');
 });
 
 app.get('/over/:num([0-9]+)', function(req, res) {
-  res.send('over ' + req.params.num);
+
+  res.header('Content-Type', 'text/xml; charset=utf-8');
+
+  var options = {
+    host: 'dirty.ru',
+    port: 80,
+    path: '/rss_7.xml'
+  };
+  http.get(options, function(innerRes) {
+    innerRes.on('data', function(chunk) {
+      res.write(chunk);
+    });
+    innerRes.on('end', function() {
+      res.end();
+    });
+  }).on('error', function(e) {
+    console.log("Got error: " + e.message);
+  });
+
 });
 
 app.listen(3000);
